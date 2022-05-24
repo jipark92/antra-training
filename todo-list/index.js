49ed0b6e85d459e6fd4f4996b1c679497dee7862
Ji Park <jiyongprk@gmail.com>
2022-05-24 19:58:51 -0400

    capitalize function

--- todo-list/index.js
+++ todo-list/index.js
@@ -13,14 +13,13 @@ const addToDo = () =>{
         clearInputValue()
         console.log(toDoList)
     })
-    
 }
 addToDo()
 
 const getToDoValue = () =>{
     toDoList.push({
         id: toDoID++,
-        title: toDoValue.value
+        title: toDoValue.value.toUpperCase()
     })
 }
 
@@ -28,40 +27,40 @@ const clearInputValue = () =>{
     toDoValue.value = "";
 }
 
-
 //CREATE HTML ELEMENTS(LIST AND DELETE BUTTON)
 const createList = () =>{
+    //create list container and grabs attribute values
     const list = document.createElement('div')
     list.setAttribute('class', 'list')
     for (let i = 0; i < toDoList.length;i++){
         list.setAttribute('id', toDoList[i].id)
-
     }
     listContainer.appendChild(list)
     const listID = list.getAttribute('id')
 
+    //create title box
     const toDoTitle = document.createElement('p')
     list.appendChild(toDoTitle)
     for(let i=0; i<toDoList.length;i++){
-        toDoTitle.textContent = toDoList[i].title
+        toDoTitle.textContent = `#${toDoList[i].id} ${toDoList[i].title}`
     }
 
+    //create delete button element
     const deleteBtn = document.createElement('button')
     deleteBtn.setAttribute('class','delete-btn')
     list.appendChild(deleteBtn)
     deleteBtn.textContent = "DELETE"
 
+    //delete function
     deleteBtn.addEventListener('click',()=>{
         console.log(listID)
         console.log(list)
-
         for (let i = 0; i < toDoList.length;i++){
             if(parseInt(listID) === toDoList[i].id){
                 toDoList.splice(i,1)
                 console.log(toDoList)
             }
             list.remove()
-
         }
     })
 }
