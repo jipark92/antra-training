d147fe1f48393d74eb4da1f3d9e4854ad84ae518
Ji Park <jiyongprk@gmail.com>
2022-05-24 08:19:17 -0400

    some method

--- prototypes/index.js
+++ prototypes/index.js
@@ -1,46 +1,42 @@
+//RANDOM ARRAY
 const arr = [5,55,12,129,595]
 
-//my filter method
-Array.prototype.MyFilter = function (cbf) {
-    const filteredArray = []
-    for (let i = 0; i < this.length; i++) {
+
+//MY FILTER METHOD
+Array.prototype.MyFilter = function(cbf){
+    const filteredArray = [];
+    for (let i = 0; i < this.length; i++){
         if(cbf(this[i], i, this)){
-            filteredArray.push(this[i])
+            filteredArray.push(this[i]);
         }
     }
-    return filteredArray
+    return filteredArray;
 }
-
-let filteredResult = arr.MyFilter(item => {
-        return  item < 50
-})
+const filteredResult = arr.MyFilter(item => item < 50)
 console.log(filteredResult); // 5,12
 
-// my reduce method
-Array.prototype.MyReduce = function (cbf) {
+
+//MY REDUCE METHOD
+Array.prototype.MyReduce = function(cbf) {
     let temp = 0;
     for (let i =0 ; i< this.length;i++){
-        cbf(temp = temp+this[i])
+        cbf(temp = temp + this[i]);
     }
-    return temp
+    return temp;
 }
+const reduceResult = arr.MyReduce((temp,item)=>item + temp);
+console.log(reduceResult);// 796 total
 
-let reduceResult = arr.MyReduce((temp,item)=>{
-    return item + temp
-},0)
-console.log(reduceResult)
 
-// // // my some method
-// Array.prototype.MySome = function (cbf) {
-//     for (let i = 0 ; i < this.lensgth; i++ ){
-//         if (this[i] === this ){
-//             return true
-//         } 
-//     }
-//     return false
-// }
-
-// let someResult = arr.MySome(people=>{
-//         return people.age === 25
-// })
-// // console.log(someResult)
+//MY SOME METHOD
+Array.prototype.MySome = function(cbf) {
+    for (let i = 0 ; i < this.length; i++ ){
+        if( typeof(this[i]) === typeof(this) ) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+const someResult = arr.MySome(arr < 5 )
+console.log(someResult) // true
