b997be40fa3ac8a7c6f44be9bd2073670bbe1969
Ji Park <jiyongprk@gmail.com>
2022-05-24 19:45:25 -0400

    delete function deletes specific array and dom

--- todo-list/index.js
+++ todo-list/index.js
@@ -2,7 +2,6 @@ const toDoValue = document.querySelector('.todo-value')
 const addToDoBtn = document.querySelector('.add-todo')
 const listContainer = document.querySelector('.list-container')
 
-
 const toDoList = []
 let toDoID = 0;
 
@@ -29,11 +28,17 @@ const clearInputValue = () =>{
     toDoValue.value = "";
 }
 
+
+//CREATE HTML ELEMENTS(LIST AND DELETE BUTTON)
 const createList = () =>{
     const list = document.createElement('div')
     list.setAttribute('class', 'list')
-    listContainer.appendChild(list)
+    for (let i = 0; i < toDoList.length;i++){
+        list.setAttribute('id', toDoList[i].id)
 
+    }
+    listContainer.appendChild(list)
+    const listID = list.getAttribute('id')
 
     const toDoTitle = document.createElement('p')
     list.appendChild(toDoTitle)
@@ -41,21 +46,22 @@ const createList = () =>{
         toDoTitle.textContent = toDoList[i].title
     }
 
-
     const deleteBtn = document.createElement('button')
     deleteBtn.setAttribute('class','delete-btn')
     list.appendChild(deleteBtn)
     deleteBtn.textContent = "DELETE"
 
     deleteBtn.addEventListener('click',()=>{
-        // console.log('delted')
-        for (let i = 0; i < toDoList.length;i++){
-            if(toDoTitle.textContent === toDoList[i].title){
-                console.log('delted')
+        console.log(listID)
+        console.log(list)
 
+        for (let i = 0; i < toDoList.length;i++){
+            if(parseInt(listID) === toDoList[i].id){
                 toDoList.splice(i,1)
                 console.log(toDoList)
             }
+            list.remove()
+
         }
     })
 }
