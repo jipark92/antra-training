49fc96d21a7d235b5933f855c821ef63b3236d3e
Ji Park <jiyongprk@gmail.com>
2022-06-01 16:57:24 -0400

    interview assignment

--- evaluation/index.js
+++ evaluation/index.js
@@ -10,7 +10,7 @@ let num = albumArray.length - 20
 const showMore = () =>{ 
     showMoreBtn.addEventListener('click',()=>{
         console.log('showmore')
-        if(-num !== albumArray.length){
+        if(-num < albumArray.length){
             num = num - 20
         } else{
             alert('end of page')
