45ddaef3ab24480ebe4e9d06bb366f6da11476b9
Ji Park <jiyongprk@gmail.com>
2022-06-14 13:57:36 -0400

    initial commit

--- advanced_training/webpack/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export function App() {
-    return (
-        <div>
-            <h1>Hello Antra</h1>
-        </div>
-    )
-}
--- advanced_training/webpack/webpack.config.js
+++ advanced_training/webpack/webpack.config.js
@@ -1,87 +1,9 @@
-const webpack = require("webpack")
-const path = require("path")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require('path');
 
-module.exports = (env, options) => {
-
-    return {
-        target: "web",
-        entry: "./src/index.js",
-        output: {
-            filename: "main.js",
-            path: path.join(__dirname, "/dist"),
-        },
-        module: {
-            rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                loader: "babel-loader",
-                options: {
-                    cacheDirectory: true,
-                },
-                },
-            },
-            ],
-        },
-        optimization: {
-            runtimeChunk: {
-            name: "runtime",
-            },
-            splitChunks: {
-            chunks: "all",
-            cacheGroups: {
-                defaultVendors: {
-                name: "vendors",
-                test: /[\\/]node_modules[\\/]/,
-                },
-            },
-            name: false,
-            },
-        },
-        performance: {
-            hints: false,
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-            minify: {
-                    collapseWhitespace: true,
-                    keepClosingSlash: true,
-                    minifyCSS: true,
-                    minifyJS: true,
-                    minifyURLs: true,
-                    removeComments: true,
-                    removeEmptyAttributes: true,
-                    removeRedundantAttributes: true,
-                    removeScriptTypeAttributes: true,
-                    removeStyleLinkTypeAttributes: true,
-                    useShortDoctype: true,
-                    },
-            template: "./src/index.html",
-            }),
-        ],
-        stats: {
-            assetsSort: "!size",
-            colors: true,
-            entrypoints: false,
-            errors: true,
-            errorDetails: true,
-            groupAssetsByChunk: false,
-            groupAssetsByExtension: false,
-            groupAssetsByInfo: false,
-            groupAssetsByPath: false,
-            modules: false,
-            relatedAssets: true,
-            timings: false,
-            version: false,
-        },
-        devServer: {
-            contentBase: "dist",
-            historyApiFallback: true,
-            host: "0.0.0.0",
-            hot: true,
-            port: 8080,
-        },
-    }
-}
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
