035afa0997886c7fd1d28821153b9d7e99f4b500
Ji Park <jiyongprk@gmail.com>
2022-06-14 19:49:22 -0400

    initial commit

---
+++ advanced_training/react-functional/src/myReact/MyReactDom.js
@@ -0,0 +1,86 @@
+import React from 'react';
+
+const render = (
+    reactElement,
+    domElement
+) => {
+    let curDom;
+    console.log("TEST", reactElement)
+    if (reactElement === undefined) {
+        return;
+    }
+    if (typeof reactElement === 'string' | typeof reactElement === 'number') {
+        curDom =
+            document.createTextNode(reactElement);
+    } else {
+        const { type, props } = reactElement;
+        /// if type is ClassCompoennt
+        if (type.prototype instanceof React.Component) {
+            // console.log('class componnent props', props)
+            /// Mounting
+            /// constructor
+            const curInstance = new type(props);
+            // console.log("curInstance", curInstance)
+
+            // getDerivedStateFromProps
+            curInstance.state = type.getDerivedStateFromProps(props, curInstance.state)
+            // console.log("curInstance", curInstance)
+
+            // render
+            const curReactElement = curInstance.render();
+            console.log("curReactElement", curReactElement);
+            render(curReactElement, domElement);
+            if (curInstance.componentDidmount) {
+                curInstance.componentDidmount()
+            }
+            return
+        }
+
+        // Assignment if it is function component
+        var template = '<h1>Hello world!</h1>';
+
+        var render = function (template, selector) {
+            var node = document.querySelector(selector);
+            if (!node) return;
+            node.innerHTML = template;
+        };
+        render(template, '#main');
+
+
+        /// else 
+        curDom = document.createElement(type);
+        Object.entries(props).forEach(
+            ([key, value]) => {
+                if (key === 'children') {
+                    if (Array.isArray(value)) {
+                        console.log(value);
+                        value.forEach((rElement) => {
+                            render(rElement, curDom);
+                        });
+                    } else {
+                        render(value, curDom);
+                    }
+                } else if (key.startsWith('on')) {
+                    curDom.addEventListener(
+                        getEventActionFromProps(key),
+                        value
+                    );
+                } else {
+                    curDom[key] = value;
+                }
+            }
+        );
+    }
+    domElement.appendChild(curDom);
+};
+
+//utils
+function getEventActionFromProps(propsKey) {
+    return propsKey.slice(2).toLowerCase();
+}
+
+const MyReactDOM = {
+    render: render
+}
+
+export default MyReactDOM
