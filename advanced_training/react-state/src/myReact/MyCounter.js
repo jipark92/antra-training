727205dc5b2053b58d64a05441892ba0be21aee9
Ji Park <jiyongprk@gmail.com>
2022-06-16 08:55:41 -0400

    update

--- advanced_training/react-state/src/myReact/MyCounter.js
+++ advanced_training/react-state/src/myReact/MyCounter.js
@@ -5,8 +5,6 @@ export default function Counter() {
 
     const [counter,SetCounter] = useState(0)
 
-
-
     const add = () =>{
         console.log('add')
         SetCounter(prevCounter => prevCounter + 1)
@@ -17,9 +15,13 @@ export default function Counter() {
         SetCounter(prevCounter => prevCounter - 1)
     }
 
+    if(counter > 5){
+        alert('alert after 5 counter')
+    }
+
     return (
         <div>
-            <p>Counter 0 </p>
+            <p>Counter {counter} </p>
             <button onClick={add}>+</button>
             <button onClick={sub}>-</button>
             <button>Alert after 5</button>
