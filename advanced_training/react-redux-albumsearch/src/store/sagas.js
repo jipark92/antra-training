6cc065f32d548bd09d029142cc3ede702d23b48c
Ji Park <jiyongprk@gmail.com>
2022-06-24 16:47:50 -0400

    update

--- advanced_training/react-redux-albumsearch/src/store/sagas.js
+++ advanced_training/react-redux-albumsearch/src/store/sagas.js
@@ -1,7 +1,7 @@
 import { call, put, takeEvery} from 'redux-saga/effects'
 import { GET_ALBUM_FETCH, GET_ALBUM_SUCCESS} from './actions'
 
-function usersFetch(){
+function albumsFetch(){
     return fetch('https://itunes.apple.com/search?term=gaga&media=music&entity=album&attribute=artistTerm&limit=200')
     .then(res=>res.json())
     .then(res=>res.results
@@ -9,7 +9,7 @@ function usersFetch(){
 }
 
 function* workGetAlbumFetch(){
-    const album = yield call(usersFetch)
+    const album = yield call(albumsFetch)
     yield put({type: GET_ALBUM_SUCCESS, album})
 }
 
