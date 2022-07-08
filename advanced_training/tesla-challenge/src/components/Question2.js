4991cf31fd746a866d254e0650c65983012c1f31
Ji Park <jiyongprk@gmail.com>
2022-07-08 19:01:25 -0400

    update

--- advanced_training/tesla-challenge/src/components/Question2.js
+++ advanced_training/tesla-challenge/src/components/Question2.js
@@ -4,91 +4,78 @@ import teslaData from '../teslaData'
 export default function Question2() {
     const [regionSelect, setRegionSelect] = useState("");
     const [modelSelect, setModelSelect] = useState("");
-    
-        const allData = teslaData.map((data, i) => {
-        return (
-            <tr key={i}>
-            <td>{data.region}</td>
-            <td>{data.model}</td>
-            <td>{data.sales}</td>
-            </tr>
-        );
-        });
-    
-        const regionFilter = teslaData.map((data, i) => {
-        if (data.region === regionSelect || data.model === modelSelect) {
-            return (
+
+    const defaultTable = teslaData.map((data,i)=>{
+        return(
             <tr key={i}>
                 <td>{data.region}</td>
                 <td>{data.model}</td>
                 <td>{data.sales}</td>
             </tr>
-            );
-        } else if (regionSelect === "ALL" || modelSelect === "ALL") {
-            return (
+        )
+    })
+    
+    const filteredTable = teslaData.filter(data=>{
+
+        const {region,model,sales} = data
+
+        if(region === regionSelect || model === modelSelect){
+            return region
+        }
+        if(regionSelect === "ALL" || modelSelect === "ALL"){
+            return region
+        }
+    }).map((data,i)=>{
+        return (
             <tr key={i}>
                 <td>{data.region}</td>
                 <td>{data.model}</td>
                 <td>{data.sales}</td>
             </tr>
-            );
+        )
+    })
+
+    const [renderTable, setRenderTable] = useState(defaultTable)
+
+    useEffect(()=>{
+        if(modelSelect||regionSelect){
+            setRenderTable(filteredTable)
         }
-        });
-    
-        // const modelFilter = teslaData.map((data,i)=>{
-    
-        // })
-    
-        useEffect(() => {
-        if (regionSelect || modelSelect) {
-            setDisplayData(regionFilter);
-        } else if (!regionSelect) {
-            setDisplayData(allData);
+        if(!modelSelect&&!regionSelect){
+            setRenderTable(defaultTable)
         }
-    
-        // sumFn();
-        }, [regionSelect, modelSelect]);
-    
-        const [displayData, setDisplayData] = useState(allData);
-    
-        return (
-        <div className="x">
-            <div className="selection-box">
-            {/* region filter */}
-            <p>Region: </p>
+    },[regionSelect,modelSelect])
+
+    return (
+        <div>
             <select
-                onChange={(e) => {
-                setRegionSelect(e.currentTarget.value);
-                }}
-            >
+                onChange={(e) => {setRegionSelect(e.currentTarget.value)}}
+                >
                 <option value="ALL">ALL</option>
                 <option value="US">US</option>
                 <option value="EU">EU</option>
                 <option value="CA">CA</option>
             </select>
-            {/* model filter */}
-            <p>Model: </p>
             <select
                 onChange={(e) => {
-                setModelSelect(e.currentTarget.value);
+                    setModelSelect(e.currentTarget.value);
                 }}
-            >
+                >
                 <option value="ALL">ALL</option>
                 <option value="A">A</option>
                 <option value="B">B</option>
                 <option value="C">C</option>
                 <option value="D">D</option>
             </select>
-            </div>
             <table border="1">
-            <tbody>
+                <tbody>
                 <tr>
-                <th>Region</th>
-                <th>Model</th>
-                <th>Sales</th>
+                    <th>Region</th>
+                    <th>Model</th>
+                    <th>Sales</th>
                 </tr>
-                {displayData}
-            </tbody>
+                {renderTable}
+                </tbody>
             </table>
         </div>
     );
