0dc19677aae1f2a0909dc4c15a208b9b3f9e967c
Ji Park <jiyongprk@gmail.com>
2022-07-08 21:17:56 -0400

    clean up

--- advanced_training/tesla-challenge/src/components/Question1.js
+++ advanced_training/tesla-challenge/src/components/Question1.js
@@ -4,57 +4,57 @@ import teslaData from "../teslaData";
 export default function Question1() {
     const getTable = () => {
         return teslaData.map((data, i) => {
-        const { region, model, sales } = data;
-        return (
-            <tr key={i}>
-            <td>{region}</td>
-            <td>{model}</td>
-            <td>{sales}</td>
-            </tr>
-        );
+            const { region, model, sales } = data;
+            return (
+                <tr key={i}>
+                    <td>{region}</td>
+                    <td>{model}</td>
+                    <td>{sales}</td>
+                </tr>
+            );
         });
     };
 
     let sum = teslaData.reduce(
         (total, data) => {
-        const { region, sales } = data;
-        if (region === "US") total.us += sales;
-        if (region === "EU") total.eu += sales;
-        if (region === "CA") total.ca += sales;
-        return total;
+            const { region, sales } = data;
+            if (region === "US") total.us += sales;
+            if (region === "EU") total.eu += sales;
+            if (region === "CA") total.ca += sales;
+            return total;
         },
         {
-        us: 0,
-        eu: 0,
-        ca: 0
+            us: 0,
+            eu: 0,
+            ca: 0,
         }
     );
 
     return (
-        <table border="1">
-        <tbody>
-            <tr>
-            <th>Region</th>
-            <th>Model</th>
-            <th>Sales</th>
-            </tr>
-            {getTable()}
-            <tr>
-            <td>US</td>
-            <td>SUM</td>
-            <td>{sum.us}</td>
-            </tr>
-            <tr>
-            <td>EU</td>
-            <td>SUM</td>
-            <td>{sum.eu}</td>
-            </tr>
-            <tr>
-            <td>CA</td>
-            <td>SUM</td>
-            <td>{sum.ca}</td>
-            </tr>
-        </tbody>
+        <table border='1'>
+            <tbody>
+                <tr>
+                    <th>Region</th>
+                    <th>Model</th>
+                    <th>Sales</th>
+                </tr>
+                {getTable()}
+                <tr>
+                    <td>US</td>
+                    <td>SUM</td>
+                    <td>{sum.us}</td>
+                </tr>
+                <tr>
+                    <td>EU</td>
+                    <td>SUM</td>
+                    <td>{sum.eu}</td>
+                </tr>
+                <tr>
+                    <td>CA</td>
+                    <td>SUM</td>
+                    <td>{sum.ca}</td>
+                </tr>
+            </tbody>
         </table>
     );
 }
--- advanced_training/tesla-challenge/src/components/Question2.js
+++ advanced_training/tesla-challenge/src/components/Question2.js
@@ -7,31 +7,31 @@ export default function Question2() {
 
     const defaultTable = teslaData.map((data, i) => {
         return (
-        <tr key={i}>
-            <td>{data.region}</td>
-            <td>{data.model}</td>
-            <td>{data.sales}</td>
-        </tr>
+            <tr key={i}>
+                <td>{data.region}</td>
+                <td>{data.model}</td>
+                <td>{data.sales}</td>
+            </tr>
         );
     });
 
     const filteredTable = teslaData
         .filter((data) => {
-        const { region, model } = data;
+            const { region, model } = data;
 
-        if (regionSelect === "ALL" && modelSelect === "ALL") return data;
-        if (region === regionSelect && modelSelect === "ALL") return data;
-        if (regionSelect === "ALL" && model === modelSelect) return data;
-        if (region === regionSelect && model === modelSelect) return data;
+            if (regionSelect === "ALL" && modelSelect === "ALL") return data;
+            if (region === regionSelect && modelSelect === "ALL") return data;
+            if (regionSelect === "ALL" && model === modelSelect) return data;
+            if (region === regionSelect && model === modelSelect) return data;
         })
         .map((data, i) => {
-        return (
-            <tr key={i}>
-            <td>{data.region}</td>
-            <td>{data.model}</td>
-            <td>{data.sales}</td>
-            </tr>
-        );
+            return (
+                <tr key={i}>
+                    <td>{data.region}</td>
+                    <td>{data.model}</td>
+                    <td>{data.sales}</td>
+                </tr>
+            );
         });
 
     const [renderTable, setRenderTable] = useState(defaultTable);
@@ -41,46 +41,46 @@ export default function Question2() {
     }, [regionSelect, modelSelect]);
 
     return (
-        <div className="App">
-        <div className="option-container">
-            <label htmlFor="region">Region</label>
-            <select
-            name="region"
-            id="region"
-            onChange={(e) => {
-                setRegionSelect(e.currentTarget.value);
-            }}
-            >
-            <option value="ALL">ALL</option>
-            <option value="US">US</option>
-            <option value="EU">EU</option>
-            <option value="CA">CA</option>
-            </select>
-            <label htmlFor="model">Model</label>
-            <select
-            name="model"
-            id="model"
-            onChange={(e) => {
-                setModelSelect(e.currentTarget.value);
-            }}
-            >
-            <option value="ALL">ALL</option>
-            <option value="A">A</option>
-            <option value="B">B</option>
-            <option value="C">C</option>
-            <option value="D">D</option>
-            </select>
-        </div>
-        <table border="1">
-            <tbody>
-            <tr>
-                <th>Region</th>
-                <th>Model</th>
-                <th>Sales</th>
-            </tr>
-            {renderTable}
-            </tbody>
-        </table>
+        <div className='App'>
+            <div className='option-container'>
+                <label htmlFor='region'>Region</label>
+                <select
+                    name='region'
+                    id='region'
+                    onChange={(e) => {
+                        setRegionSelect(e.currentTarget.value);
+                    }}
+                >
+                    <option value='ALL'>ALL</option>
+                    <option value='US'>US</option>
+                    <option value='EU'>EU</option>
+                    <option value='CA'>CA</option>
+                </select>
+                <label htmlFor='model'>Model</label>
+                <select
+                    name='model'
+                    id='model'
+                    onChange={(e) => {
+                        setModelSelect(e.currentTarget.value);
+                    }}
+                >
+                    <option value='ALL'>ALL</option>
+                    <option value='A'>A</option>
+                    <option value='B'>B</option>
+                    <option value='C'>C</option>
+                    <option value='D'>D</option>
+                </select>
+            </div>
+            <table border='1'>
+                <tbody>
+                    <tr>
+                        <th>Region</th>
+                        <th>Model</th>
+                        <th>Sales</th>
+                    </tr>
+                    {renderTable}
+                </tbody>
+            </table>
         </div>
     );
 }
